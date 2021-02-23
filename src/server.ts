4bf537a7c4e965a5c186158ad0f2f1d7bf24817d
= <=>
2021-02-23 12:40:38 -0300

    Day 2 - Configuração inicial de Database

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,11 @@
+import "reflect-metadata";
 import express from 'express';
+import "./database";
+import { router } from './routes';
 
 const app = express();
 
-// http://localhost:3333/
-app.get("/", (req, res) => {
-
-    return res.json({message: 'Hello World - NLW04'})
-});
-
-// 1º param: ROTA (RECURSO API)
-// 2º param: request,response
-app.post("/", (req, res) =>{
-    //Recebeu os dados para salvar
-
-return res.json({message: "Os dados foram salvos com sucesso!"});
-});
-
-
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log("Server is running"));
